a65b552ad3a2e1719ba6cb3100065a23c911ddf7
light-ai <yoneda.yuki785@mail.kyutech.jp>
2020-11-19 20:35:04 +0900

    get quest data

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,10 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtubepractice/quest.dart';
 
 class MainModel extends ChangeNotifier{
   String lightText = "wwwwww";
+  List<Quest> quest = [];
+
+  Future questList() async{
+    final snapshot = await FirebaseFirestore.instance.collection("quest").get();
+    final docs = snapshot.docs;
+    final quest = docs.map((doc) => Quest(doc)).toList();
+    this.quest = quest;
+    notifyListeners();
+  }
 
   void changeLightText() {
     lightText = "ああああああ";
     notifyListeners();
   }
 }
+
